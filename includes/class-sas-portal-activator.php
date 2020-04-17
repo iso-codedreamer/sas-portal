9f07f04b2ebea9f132c2f3a6903808362cfd72f7
Isack Shayo <isack.it@gmail.com>
2020-04-17 16:44:03 +0300

    Added activation handler for plugin tables

--- includes/class-sas-portal-activator.php
+++ includes/class-sas-portal-activator.php
@@ -30,7 +30,55 @@ class SAS_Portal_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $table_prefix, $wpdb;
 
+		$sql = <<<SQL
+CREATE TABLE `sas_files` (
+  `file_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `filename` text,
+  `filemime` text,
+  `filecontent` longblob,
+  `notes` text,
+  `subject` varchar(50) DEFAULT NULL,
+  `upload_date` int(10) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `sas_files_classes` (
+  `file_id` int(10) UNSIGNED NOT NULL,
+  `class` varchar(15) NOT NULL,
+  PRIMARY KEY (`file_id`,`class`) USING BTREE,
+  FOREIGN KEY (`file_id`) REFERENCES `sas_files` (`file_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `sas_issued_passwords` (
+  `phone` varchar(16) NOT NULL,
+  `issued_time` int(10) UNSIGNED DEFAULT NULL,
+  `hash` text,
+  PRIMARY KEY (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `sas_student_data` (
+  `reg_num` varchar(20) NOT NULL,
+  `names` varchar(100) NOT NULL,
+  `class` varchar(15) DEFAULT NULL,
+  `data` mediumtext,
+  PRIMARY KEY (`reg_num`),
+  KEY `class` (`class`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `sas_phones` (
+  `phone` varchar(16) NOT NULL,
+  `reg_num` varchar(20) NOT NULL,
+  PRIMARY KEY (`reg_num`,`phone`),
+  FOREIGN KEY (`reg_num`) REFERENCES `sas_student_data` (`reg_num`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SQL;
+
+
+		require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+		dbDelta($sql);
 	}
 
 }
